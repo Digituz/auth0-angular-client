0709901bc3d820e1965f4988ee62874a969d5f0e
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-27 18:40:20 -0200

    Removing hard-coded localhost value

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import * as auth0 from 'auth0-js';
 import {Router} from '@angular/router';
+import {DOCUMENT} from '@angular/common';
 
 @Injectable()
 export class AuthService {
@@ -12,14 +13,14 @@ export class AuthService {
     audience: 'http://spring-boot-aside.auth0samples.com/',
     scope: 'read:contacts',
     responseType: 'token id_token',
-    redirectUri: 'http://localhost:4200/callback'
+    redirectUri: `${this.document.location.origin}/callback`
   };
 
   // Create a stream of logged in status to communicate throughout app
   loggedIn: boolean;
   loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
 
-  constructor(private router: Router) {
+  constructor(private router: Router,  @Inject(DOCUMENT) private document: any) {
     // If authenticated, update login status subject
     if (this.authenticated) {
       this.setLoggedIn(true);
