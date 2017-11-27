8461b75c0b1cb131926cc99e9521c069bf82ad05
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-27 17:34:07 -0200

    enabling users to logout, improving layout, and enabling users to send a few types of request

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthService} from '../auth/auth.service';
 import {Router} from '@angular/router';
+import {DOCUMENT} from '@angular/common';
+import jwt_decode from 'jwt-decode';
 
 export interface Request {
   url: string,
@@ -14,17 +16,17 @@ export interface Request {
   templateUrl: './http-client.component.html',
   styleUrls: ['./http-client.component.css']
 })
-export class HttpClientComponent implements OnInit {
+export class HttpClientComponent {
   req: Request = {
     url: 'http://localhost:8080/contacts/',
-    method: 'get',
+    method: 'post',
     response: null
   };
+  scope: string;
 
-  constructor(public router: Router, private http: HttpClient, private auth: AuthService) {
-  }
-
-  ngOnInit() {
+  constructor(public router: Router, private http: HttpClient,
+              public auth: AuthService, @Inject(DOCUMENT) public document: any) {
+    this.scope = jwt_decode(localStorage.getItem('access_token')).scope;
   }
 
   changeMethod(newMethod: string) {
@@ -32,14 +34,33 @@ export class HttpClientComponent implements OnInit {
   }
 
   sendRequest() {
-    const method = this.http[this.req.method];
     const accessToken = localStorage.getItem('access_token');
-    this.http.get(this.req.url, {
+    const requestOptions = {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + accessToken),
-    }).subscribe(data => {
+    };
+    const {url} = this.req;
+    const loadResponse = data => {
       this.req.response = data;
-    });
-    // method(this.req.url).subscribe(data => {
-    // });
+    };
+
+    switch (this.req.method) {
+      case 'get':
+        this.http.get(url, requestOptions).subscribe(loadResponse);
+        break;
+      case 'post':
+        this.http.post(url, {}, requestOptions).subscribe(loadResponse);
+        break;
+      case 'put':
+        this.http.put(url, {}, requestOptions).subscribe(loadResponse);
+        break;
+      case 'delete':
+        this.http.delete(url, requestOptions).subscribe(loadResponse);
+        break;
+    }
+  }
+
+  signOut() {
+    this.auth.logout();
+    this.router.navigate(['/']);
   }
 }
