c55b010785cbf189bc6f621fd37d66f37c2d7613
Bruno Krebs <bruno.krebs@auth0.com>
2017-11-27 16:17:04 -0200

    Sending HTTP GET requests.

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -1,7 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthService} from '../auth/auth.service';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
+
+export interface Request {
+  url: string,
+  method: string
+  response: object
+}
 
 @Component({
   selector: 'app-http-client',
@@ -9,7 +15,11 @@ import {Router} from "@angular/router";
   styleUrls: ['./http-client.component.css']
 })
 export class HttpClientComponent implements OnInit {
-  httpResponse: string;
+  req: Request = {
+    url: 'http://localhost:8080/contacts/',
+    method: 'get',
+    response: null
+  };
 
   constructor(public router: Router, private http: HttpClient, private auth: AuthService) {
   }
@@ -17,4 +27,19 @@ export class HttpClientComponent implements OnInit {
   ngOnInit() {
   }
 
+  changeMethod(newMethod: string) {
+    this.req.method = newMethod;
+  }
+
+  sendRequest() {
+    const method = this.http[this.req.method];
+    const accessToken = localStorage.getItem('access_token');
+    this.http.get(this.req.url, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + accessToken),
+    }).subscribe(data => {
+      this.req.response = data;
+    });
+    // method(this.req.url).subscribe(data => {
+    // });
+  }
 }
